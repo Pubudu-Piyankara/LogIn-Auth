a362a34571334ca12af0f939958bff7635ca818b
Piyankara N W M G P <pubudupiyankara.me@gmail.com>
2023-12-10 08:05:59 +0530

    connect mongodb

--- backend/models/userSchema.js
+++ backend/models/userSchema.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
 
-const userSchema1 = new mongoose.Schema({
+const userSchema  = new mongoose.Schema({
     email:{type: String, required: true},
     username : {type:String, required: true, unique : true},
     password :{type:String, required:true},
 })
 
-const User = mongoose.model,{'User', userSchema1}
+const User = mongoose.model('User', userSchema)
 
 module.exports =User
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose =  require('mongoose')
 const cors =  require('cors')
 const bodyParser = require('body-parser')
-const bycrypt = require('bcrypt')
+const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const userSchema1 = require('./models/userSchema')
 
@@ -10,27 +10,38 @@ const userSchema1 = require('./models/userSchema')
 const app= express()
 
 //connect to MongoDB
-const dbURL ='mongodb+srv://pubudu:pubudu99@cluster99.xcnka7n.mongodb.net/UserDB?retryWrites=true&w=majority'
+const dbURL ='mongodb+srv://pubu:test123@cluster99.xcnka7n.mongodb.net/?retryWrites=true&w=majority'
 mongoose.connect(dbURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true
 })
-.then(()=>{
-    app.listen(3001, ()=>{
+.then(()=>{ 
+    app.listen(3001, ()=>{ 
 
         console.log('Server is connected to port 3001 and MongoDB')
     })
 })
-.catch((error)=>{
-    console.log('Unable to connect to server')
+.catch((error)=>{ 
+    console.log('Unable to connect to server; ', error)
 })
 
+
 //middleware
-app.use(bodyparser.json())
+app.use(bodyParser.json())
 app.use(cors())
 
 //schema
 
 
 //routes
+//user registration
+//post register
+app.post('/register', (req,res)=>{
+    try{
+        const{email, username, password} = res.body
+    }
+    catch{
+        
+    }
+})
 
